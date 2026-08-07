9b181e83cfeaa74d68fb684c485c12d1063b56f8
apple <apple@apples-MacBook-Air.local>
2026-08-07 10:26:01 +0900

    chore: remove sample module

--- src/modules/sample/sample.service.ts
+++
@@ -1,9 +0,0 @@
-import { sampleRepository } from './sample.repository';
-
-export const findAllSamples = async () => {
-  return sampleRepository.findAll();
-};
-
-export const findSampleById = async (id: number) => {
-  return sampleRepository.findById(id);
-};
